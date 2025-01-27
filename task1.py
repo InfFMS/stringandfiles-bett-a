e3eb322ec159399fcf45a58224e196b2caed15e8
Лаврова Елизавета Александровна <e.lavrova2@main.nsu.ru>
2025-01-27 12:39:25 +0700

    1,2,3,4

--- task1.py
+++ task1.py
@@ -1,6 +1,29 @@
 # Откройте текстовый файл для чтения task1.txt.
 # Подсчитайте:
-# Общее количество строк в файле.
+# Общее количество строк в файле. +
 # Общее количество слов во всем тексте файла.
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
+
+lines = 0
+words = 0
+symbols = 0
+l = []
+with open('task1.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        lines += 1
+        l.append(line)
+
+s = l[0] + l[1] + l[2]
+symbols = len(s)
+new_s = s.replace('.', '').split()
+for j in new_s:
+    if j.isalpha():
+        words += 1
+
+print('Кол-во линий: ', lines)
+print('Кол-во слов: ', words)
+print('Кол-во символов: ', symbols)
+
+
+
--- task2.py
+++ task2.py
@@ -3,3 +3,14 @@
 # Найдите и замените все вхождения слова "Python" на слово "Питон" (регистр учитывать).
 # Запишите обновленный текст в новый файл с другим именем.
 # Выведите на экран сообщение о количестве произведённых замен.
+
+with open('task2.txt', 'r', encoding='utf-8') as f:
+    file = f.read()
+
+changes = file.count('Python')
+file = file.replace('Python', 'Питон')
+with open('edited_task2.txt', 'w', encoding='utf-8') as d:
+    d.write(file)
+
+print('Замен произведено: ', changes)
+#print(file)
